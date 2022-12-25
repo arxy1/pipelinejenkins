c89887d1934becd7098c4d5d0b26c1ca944cab00
aircryptx <netizba@gmail.com>
2022-12-25 12:07:39 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Well done devops777!"));
+app.get("/", (req, res) => res.send("Well done devops7777!"));
 
 app.get("/health", (req, res) => {
   res.status(200);
